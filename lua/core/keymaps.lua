0cc9dc400d788a2954512c635c15148fee1564f5
Ricardo Mora <rurimoca@gmail.com>
2024-12-06 05:44:50 -0600

    NERDTreeToggle commented

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,9 +1,9 @@
 -- Mapeos personalizados
 vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-b>", ":NERDTreeToggle<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-M-Left>", "<C-w>h", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-M-Down>", "<C-w>j", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-M-Up>", "<C-w>k", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-M-Right>", "<C-w>l", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true })
 
+--vim.api.nvim_set_keymap("n", "<C-b>", ":NERDTreeToggle<CR>", { noremap = true })
