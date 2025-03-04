e09332e8b908212634f08bcc3d6ed9cb01873338
Ricardo Mora <rurimoca@gmail.com>
2025-03-03 22:38:02 -0600

    Actualiza configuración de tema

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -1,5 +1,4 @@
 -- Configuración del esquema de colores
-vim.g.base16_background_transparent = 1
 vim.o.termguicolors = true
 vim.cmd [[ colorscheme base16-tokyo-city-terminal-dark ]]
 
--- lua/core/plugin_config/themes.lua
+++ lua/core/plugin_config/themes.lua
@@ -1,4 +1,4 @@
 -- Tema y colores
-vim.g.base16_background_transparent = 1
+vim.g.tinted_background_transparent = 1
 vim.cmd('colorscheme base16-tokyo-city-terminal-dark')
 
