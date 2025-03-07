d043ca08912fc7a24138f99b1a3142b9ffd4c470
Ricardo Mora <rurimoca@gmail.com>
2025-03-07 07:14:33 -0600

    Cambio de selección para sugerencias

--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -49,8 +49,8 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     -- Navigate between completion items
-    ['<TAB>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<S-TAB>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<S-TAB>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<TAB>'] = cmp.mapping.select_next_item({behavior = 'select'}),
 
     -- `Enter` key to confirm completion
     ['<CR>'] = cmp.mapping.confirm({select = false}),
