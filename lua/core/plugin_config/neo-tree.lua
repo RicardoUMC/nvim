b6f743bc14c6c09d2c8593b7308421f0e99b79a9
Ricardo Mora <rurimoca@gmail.com>
2025-03-12 04:48:36 -0600

    Agrega archivo lua para neo-tree

---
+++ lua/core/plugin_config/neo-tree.lua
@@ -0,0 +1,8 @@
+require("neo-tree").setup({
+  filesystem = {
+    follow_current_file = true,
+    use_libuv_file_watcher = true,
+  },
+})
+
+vim.keymap.set("n", "<C-n>", ":Neotree toggle=true filesystem reveal left<CR>", {})
