a4c89512e19a0605461d247d1a7037b46d3c9b89
Ricardo Mora <rurimoca@gmail.com>
2025-03-12 21:26:07 -0600

    Regresa a configuración por defecto

--- lua/core/plugin_config/neo-tree.lua
+++ lua/core/plugin_config/neo-tree.lua
@@ -3,12 +3,6 @@ require("neo-tree").setup({
         follow_current_file = true,
         use_libuv_file_watcher = true,
     },
-    default_component_configs = {
-        indent = {
-            padding = 0,
-            indent_size = 4,
-        }
-    }
 })
 
 vim.keymap.set("n", "<C-n>", ":Neotree<CR>", {})
