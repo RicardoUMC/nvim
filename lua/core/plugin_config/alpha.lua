151dbf6f3b827240a6b65b36437e018d675516f3
Ricardo Mora <rurimoca@gmail.com>
2025-03-13 00:15:11 -0600

    Configura botones

--- lua/core/plugin_config/alpha.lua
+++ lua/core/plugin_config/alpha.lua
@@ -1,7 +1,7 @@
-local alpha = require('alpha')
+local alpha = require("alpha")
 
-local dashboard = require('alpha.themes.theta')
-dashboard.header.val = {
+local theta = require("alpha.themes.theta")
+theta.header.val = {
 	[[                                                                       ]],
 	[[                                                                       ]],
 	[[                                                                       ]],
@@ -19,4 +19,17 @@ dashboard.header.val = {
 	[[                                                                       ]],
 }
 
-alpha.setup(dashboard.config)
+local dashboard = require("alpha.themes.dashboard")
+theta.buttons.val = {
+	{ type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+	{ type = "padding", val = 1 },
+	dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+	dashboard.button("SPC SPC", "󰊄  Recently opened files"),
+	dashboard.button("SPC p f", "󰈞  Find file"),
+	dashboard.button("SPC p s", "󰈬  Find word"),
+	dashboard.button("SPC s l", "  Open last session", "<cmd>cd stdpath('Local')<CR>"),
+	dashboard.button("u", "  Update plugins", "<cmd>Pckr sync<CR>"),
+	dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+}
+
+alpha.setup(theta.config)
