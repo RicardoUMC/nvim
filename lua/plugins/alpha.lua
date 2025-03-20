fca02d0b244abf3698ac538a0d2e51f5afc37df0
Ricardo Mora <rurimoca@gmail.com>
2025-03-20 11:06:25 -0600

    Configura pantalla de inicio con Alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -31,13 +31,14 @@ return {
 			theta.buttons.val = {
 				{ type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
 				{ type = "padding", val = 1 },
-				dashboard.button("e", "  New file", "<cmd>ene<CR>"),
-				dashboard.button("SPC SPC", "󰊄  Recently opened files"),
+				dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+				dashboard.button("SPC SPC", "󱋡  Recently opened files"),
 				dashboard.button("SPC p f", "󰈞  Find file"),
 				dashboard.button("SPC p s", "󰈬  Find word"),
-				dashboard.button("SPC s l", "  Open last session", "<cmd>cd stdpath('Local')<CR>"),
-				dashboard.button("u", "  Update plugins", "<cmd>Pckr sync<CR>"),
-				dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+				dashboard.button("SPC o s", "  Open session"),
+				dashboard.button("SPC l s", "  Open last session"),
+				dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
+				dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
 			}
 
 			alpha.setup(theta.config)
