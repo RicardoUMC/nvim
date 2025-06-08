3d658e3caed8784dff72583fb9d9c2eb17c642e4
Ricardo Mora <rurimoca@gmail.com>
2025-06-08 17:08:35 -0600

    Agrega plugin para saldo de palabras

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -3,10 +3,19 @@ return {
         --"jiangmiao/auto-pairs",
     },
     {
+        "chrisgrieser/nvim-spider",
+        lazy = true,
+        keys = {
+            { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+            { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+            { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+        },
+    },
+    {
         "numToStr/Comment.nvim",
         opts = {
             padding = true,
-        }
+        },
     },
     {
         "kylechui/nvim-surround",
