77f25ecd110a0c1435191706d3006833408c1307
Ricardo Mora <rurimoca@gmail.com>
2025-06-08 17:24:16 -0600

    Agrega ayuda de vim motions

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -3,6 +3,24 @@ return {
         --"jiangmiao/auto-pairs",
     },
     {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {
         "chrisgrieser/nvim-spider",
         lazy = true,
         keys = {
