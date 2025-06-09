b7d31e82083fbc1c100d8a60b1fa0ac147cc2434
Ricardo Mora <rurimoca@gmail.com>
2025-06-08 18:27:41 -0600

    Agrega ventanas flotantes

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -2,12 +2,30 @@ return {
     {
         --"jiangmiao/auto-pairs",
     },
+    -- lazy.nvim
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
+    },
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
         opts = {
             preset = "modern",
-            delay = function(_) return 0 end,
+            delay = function(_)
+                return 0
+            end,
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
