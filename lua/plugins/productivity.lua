1dc1e98800530f731c7c110987782e6db1b75e32
Ricardo Mora <rurimoca@gmail.com>
2025-06-09 22:57:48 -0600

    Agrega ventanas de notificación flotante

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -15,7 +15,14 @@ return {
             -- OPTIONAL:
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
+            {
+                "rcarriga/nvim-notify",
+                config = function()
+                    require("notify").setup({
+                        background_colour = vim.api.nvim_get_hl(0, { name = "Normal" }).bg or "#000000",
+                    })
+                end,
+            },
         },
     },
     {
