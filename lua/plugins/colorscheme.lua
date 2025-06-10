7a9176b3064a2709db1b179cd4f9035855fdab1f
Ricardo Mora <rurimoca@gmail.com>
2025-06-09 20:10:31 -0600

    Carga colorschemes al inicial nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,17 +4,11 @@ vim.g.tinted_background_transparent = 1
 return {
     {
         "rebelot/kanagawa.nvim",
-        lazy = true,
-        priority = 1000,
     },
     {
         "tinted-theming/tinted-vim",
-        lazy = true,
-        priority = 1000,
     },
     {
         "scottmckendry/cyberdream.nvim",
-        lazy = true,
-        priority = 1000,
     },
 }
