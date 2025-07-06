61311f7f19b4b316d943babdf75231f57f15b4cb
Ricardo Mora <rurimoca@gmail.com>
2025-07-06 15:18:57 -0600

    Integra agregar transparencia al lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,20 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				--theme = "ayu_dark"
-				theme = "onedark",
-				--theme = "ayu_mirage"
-				--theme = "dracula"
-				--theme = "horizon"
-			},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        local custom_onedark = require 'lualine.themes.onedark'
+        custom_onedark.normal.c.bg = "None"
+        custom_onedark.inactive.c.bg = "None"
+
+        require("lualine").setup({
+            options = {
+                icons_enabled = true,
+                theme = custom_onedark,
+                -- theme = "ayu_dark"
+                -- theme = "onedark",
+                -- theme = "ayu_mirage"
+                -- theme = "dracula"
+                -- theme = "horizon"
+            },
+        })
+    end,
 }
