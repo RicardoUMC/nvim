5accea4e69d9f6350becf36f3b29581b09992095
Ricardo Mora <rurimoca@gmail.com>
2025-07-09 22:09:22 -0600

    Reordena plugins desde el más básico al más complejo

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,55 +3,18 @@ return {
         --"jiangmiao/auto-pairs",
     },
     {
-        "folke/noice.nvim",
-        event = "VeryLazy",
+        "numToStr/Comment.nvim",
         opts = {
-            cmdline = {
-                view = "cmdline_popup", -- "cmdline" or "cmdline_popup"
-                format = {
-                    cmdline = { icon = " ", lang = "vim" },
-                    search_down = { icon = "🔍 ", lang = "regex" },
-                    search_up = { icon = "🔍 ", lang = "regex" },
-                    filter = { icon = "!", lang = "bash" },
-                },
-                presets = {
-                    bottom_search = true,
-                    command_palette = true,
-                    lsp_doc_border = true,
-                },
-            },
-        },
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            {
-                "rcarriga/nvim-notify",
-                opts = {
-                    background_colour = "#000000",
-                },
-            },
+            padding = true,
         },
     },
     {
-        "folke/which-key.nvim",
+        "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
-        opts = {
-            preset = "modern",
-            delay = function()
-                return 0
-            end,
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
+        config = function()
+            require("nvim-surround").setup({})
+        end,
     },
     {
         "chrisgrieser/nvim-spider",
@@ -66,19 +29,70 @@ return {
         },
     },
     {
-        "numToStr/Comment.nvim",
-        opts = {
-            padding = true,
-        },
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        "folke/noice.nvim",
         event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            { "rcarriga/nvim-notify", opts = { background_colour = "#000000" } },
+        },
+        opts = {
+            cmdline = {
+                view = "cmdline_popup",
+                format = {
+                    cmdline = { icon = " ", lang = "vim" },
+                    search_down = { icon = "🔍 ", lang = "regex" },
+                    search_up = { icon = "🔍 ", lang = "regex" },
+                    filter = { icon = "!", lang = "bash" },
+                },
+                presets = {
+                    bottom_search = true,
+                    command_palette = true,
+                    lsp_doc_border = true,
+                },
+            },
+        },
+        config = function(_, opts)
+            require("noice").setup(opts)
+
+            vim.api.nvim_create_autocmd("RecordingEnter", {
+                callback = function()
+                    local reg = vim.fn.reg_recording()
+                    require("noice").notify(
+                        "Recording macro: @" .. reg,
+                        vim.log.levels.INFO,
+                        { title = "Macro Recording", timeout = 2000, merge = true }
+                    )
+                end,
+            })
+
+            vim.api.nvim_create_autocmd("RecordingLeave", {
+                callback = function()
+                    require("noice").notify(
+                        "Macro recording stopped",
+                        vim.log.levels.INFO,
+                        { title = "Macro Recording", timeout = 500, merge = true }
+                    )
+                end,
             })
         end,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            preset = "modern",
+            delay = function()
+                return 0
+            end,
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
     },
 }
