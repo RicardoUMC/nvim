f641c166db95785bd203be6449af0ea68ebf6d3d
Ricardo Mora <rurimoca@gmail.com>
2025-07-09 22:15:24 -0600

    Quita fondo al horizon en configuración custom

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,11 +4,15 @@ return {
         local custom_onedark = require 'lualine.themes.onedark'
         custom_onedark.normal.c.bg = "None"
         custom_onedark.inactive.c.bg = "None"
+        local custom_horizon = require 'lualine.themes.horizon'
+        custom_horizon.normal.c.bg = "None"
+        custom_horizon.inactive.c.bg = "None"
 
         require("lualine").setup({
             options = {
                 icons_enabled = true,
                 theme = custom_onedark,
+                -- theme = custom_horizon,
                 -- theme = "ayu_dark"
                 -- theme = "onedark",
                 -- theme = "ayu_mirage"
