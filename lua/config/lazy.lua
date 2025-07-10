5499e7a7e6dc363c2511ae8fd2a7bc4ea4f59071
Ricardo Mora <rurimoca@gmail.com>
2025-07-10 00:42:37 -0600

    Corrige uso de win32yank (aún no parece funcionar)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,12 +19,12 @@ if vim.fn.has("wsl") == 1 then
     vim.g.clipboard = {
         name = "win32yank",
         copy = {
-            ["+"] = "win32yank -i --crif",
-            ["*"] = "win32yank -i --crif",
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf",
         },
         paste = {
-            ["+"] = "win32yank -o --crif",
-            ["*"] = "win32yank -o --crif",
+            ["+"] = "win32yank.exe -o --lf",
+            ["*"] = "win32yank.exe -o --lf",
         },
         cache_enabled = false,
     }
