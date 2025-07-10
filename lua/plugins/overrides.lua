6a5262288ab7948058fbcb98978cb7f89fb83dae
Ricardo Mora <rurimoca@gmail.com>
2025-07-10 00:03:54 -0600

    Agrega animaciones

---
+++ lua/plugins/overrides.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "folke/trouble.nvim",
+        opts = { use_diagnostic_signs = true },
+        cmd = "Trouble",
+    },
+    {
+        "hedyhli/outline.nvim",
+        config = function()
+            vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+            require("outline").setup({})
+        end,
+    },
+    {
+        "echasnovski/mini.nvim",
+        version = false, -- Use the latest version
+        config = function()
+            require("mini.animate").setup({
+                resize = {
+                    enable = false,
+                },
+                open = {
+                    enable = false,
+                },
+                close = {
+                    enable = false,
+                },
+                scroll = {
+                    enable = false,
+                },
+            })
+        end,
+    },
+}
