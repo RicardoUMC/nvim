f5d7716df2a2c2036de79675f45c11298f18cbd9
Ricardo Mora <rurimoca@gmail.com>
2025-07-10 22:41:13 -0600

    Modifica keymap para ir a definiciones

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -68,7 +68,7 @@ return {
                             end, { desc = "Toggle inlay hints" })
 
                             vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-                            vim.keymap.set("n", ",gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+                            vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
                             vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
                             vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
                             vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
