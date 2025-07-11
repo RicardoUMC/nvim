e66a2e2545899054d1260c9ea7ff66b7c239a5cd
Ricardo Mora <rurimoca@gmail.com>
2025-07-10 22:33:02 -0600

    Agrega capabilities a angularls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,6 +27,7 @@ return {
                             root_dir = function(fname)
                                 return require("lspconfig.util").root_pattern("angular.json", "project.json")(fname)
                             end,
+                            capabilities = capabilities,
                         },
                     },
                 },
