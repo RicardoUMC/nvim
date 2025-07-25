78906cb0e996c0fcbd25ec46b9707a13768eaae6
Ricardo Mora <rurimoca@gmail.com>
2025-07-25 00:57:08 -0600

    Agrega render opcional de markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,25 +1,25 @@
 return {
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {
-            heading = {
-                enabled = true,
-                sign = true,
-                style = "full",
-                icons = { "① ", "② ", "③ ", "④ ", "⑤ ", "⑥ " },
-                left_pad = 1,
-            },
-            bullet = {
-                enabled = true,
-                icons = { "●", "○", "◆", "◇" },
-                right_pad = 1,
-                highlight = "render-markdownBullet",
-            },
-        },
-    },
+    -- {
+    --     "MeanderingProgrammer/render-markdown.nvim",
+    --     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    --     ---@module 'render-markdown'
+    --     ---@type render.md.UserConfig
+    --     opts = {
+    --         heading = {
+    --             enabled = true,
+    --             sign = true,
+    --             style = "full",
+    --             icons = { "① ", "② ", "③ ", "④ ", "⑤ ", "⑥ " },
+    --             left_pad = 1,
+    --         },
+    --         bullet = {
+    --             enabled = true,
+    --             icons = { "●", "○", "◆", "◇" },
+    --             right_pad = 1,
+    --             highlight = "render-markdownBullet",
+    --         },
+    --     },
+    -- },
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
@@ -29,4 +29,26 @@ return {
         end,
         ft = { "markdown" },
     },
+    -- {
+    --     "OXY2DEV/markview.nvim",
+    --     priority = 49,
+    --     branch = "main",
+    --     lazy = false,
+    --     ft = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" },
+    --     init = function()
+    --         local filetypes = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" }
+    --
+    --         vim.api.nvim_create_autocmd("FileType", {
+    --             pattern = filetypes,
+    --             callback = function()
+    --                 local custom = require("markview.spec").default
+    --
+    --                 custom.preview.filetypes = filetypes
+    --                 custom.preview.ignore_buftypes = {}
+    --
+    --                 require("markview").setup(custom)
+    --             end,
+    --         })
+    --     end,
+    -- },
 }
