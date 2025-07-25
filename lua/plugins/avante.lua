19c8a7e2b2658b40180d0daed6af3c17b348ae98
Ricardo Mora <rurimoca@gmail.com>
2025-07-24 21:06:31 -0600

    Actualza a rama main para funcionamiento correcto de plugin

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,14 +2,9 @@ return {
     "yetone/avante.nvim",
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- ⚠️ must add this setting! ! !
-    build = function()
-        -- conditionally use the correct build system for the current OS
-        if vim.fn.has("win32") == 1 then
-            return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-        else
-            return "make"
-        end
-    end,
+    branch = "main",
+    build = vim.fn.has("win32") and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     ---@module 'avante'
@@ -43,14 +38,14 @@ return {
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
-        "echasnovski/mini.pick", -- for file_selector provider mini.pick
+        "echasnovski/mini.pick",         -- for file_selector provider mini.pick
         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-        "ibhagwan/fzf-lua", -- for file_selector provider fzf
-        "stevearc/dressing.nvim", -- for input provider dressing
-        "folke/snacks.nvim", -- for input provider snacks
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+        "stevearc/dressing.nvim",        -- for input provider dressing
+        "folke/snacks.nvim",             -- for input provider snacks
+        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua",        -- for providers='copilot'
         {
             -- support for image pasting
             "HakonHarnes/img-clip.nvim",
