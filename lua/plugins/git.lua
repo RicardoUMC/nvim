ace7aab09a3ae826dfa9257de6340967f2db938c
Ricardo Mora <rmorac1901@alumno.ipn.mx>
2025-11-26 13:41:13 -0600

    Agrega configuraciones y keymaps para git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,14 +1,26 @@
 return {
 	{
+		"tpope/vim-fugitive",
+		cmd = { "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "Gmove", "Gdelete", "Gedit", "Glog" },
+		keys = {
+			{ "<leader>gd", ":Gdiffsplit<CR>", desc = "Git diff split" },
+			{ "<leader>gl", ":vert leftabove Git log<CR>", desc = "Git log" },
+			{ "<leader>gc", ":Git commit -m \"\"<Left>", desc = "Commit changes" },
+		},
+	},
+	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			local gs = require("gitsigns")
-			gs.setup()
+			gs.setup({ current_line_blame = true })
 
 			vim.keymap.set("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
-			vim.keymap.set("n", "<leader>gt", gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
 			vim.keymap.set({ "n", "v" }, "<leader>ga", gs.stage_hunk, { desc = "Stage hunk" })
 			vim.keymap.set({ "n", "v" }, "<leader>gu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+            vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
+            vim.keymap.set("n", "<leader>gtn", gs.toggle_numhl, { desc = "Toggle line number highlights" })
+            vim.keymap.set("n", "<leader>gtl", gs.toggle_linehl, { desc = "Toggle highlighted lines" })
+
 		end,
 	},
 	{
