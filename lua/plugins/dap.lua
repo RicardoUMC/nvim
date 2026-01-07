fa196c56dd62421d42bc0655dc708fd8ee2b3686
Ricardo Mora <rmorac1901@alumno.ipn.mx>
2026-01-07 15:57:57 -0600

    Agrega DAP

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,144 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "theHamsta/nvim-dap-virtual-text",
+        },
+        keys = {
+            {
+                "<leader>dc",
+                function()
+                    require("dap").continue()
+                end,
+                desc = "DAP Continue",
+            },
+            {
+                "<leader>do",
+                function()
+                    require("dap").step_over()
+                end,
+                desc = "DAP Step Over",
+            },
+            {
+                "<leader>di",
+                function()
+                    require("dap").step_into()
+                end,
+                desc = "DAP Step Into",
+            },
+            {
+                "<leader>dO",
+                function()
+                    require("dap").step_out()
+                end,
+                desc = "DAP Step Out",
+            },
+            {
+                "<leader>db",
+                function()
+                    require("dap").toggle_breakpoint()
+                end,
+                desc = "DAP Toggle Breakpoint",
+            },
+            {
+                "<leader>dB",
+                function()
+                    require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+                end,
+                desc = "DAP Conditional Breakpoint",
+            },
+            {
+                "<leader>dr",
+                function()
+                    require("dap").repl.open()
+                end,
+                desc = "DAP REPL",
+            },
+            {
+                "<leader>dq",
+                function()
+                    require("dap").terminate()
+                end,
+                desc = "DAP Terminate",
+            },
+        },
+        config = function(_, _)
+            require("nvim-dap-virtual-text").setup()
+        end,
+    },
+
+    -- Mason bridge para DAP (instala adapters por nombre)
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = { "mason-org/mason.nvim", "mfussenegger/nvim-dap" },
+        cmd = { "DapInstall", "DapUninstall" },
+        opts = {
+            automatic_installation = true,
+
+            -- Aquí solo se agregan lenguajes/adapters conforme se necesiten:
+            ensure_installed = {
+                -- "delve",     -- Go
+                -- "python",    -- debugpy
+                -- "codelldb",  -- Rust/C/C++
+                -- "js",        -- depende de el entorno (ver nota abajo)
+            },
+
+            -- handlers permite “parchear” o extender configs por adapter
+            handlers = {},
+        },
+    },
+
+    -- UI: nvim-dap-view
+    {
+        "igorlfs/nvim-dap-view",
+        ---@module 'dap-view'
+        ---@type dapview.Config
+        opts = {},
+        keys = {
+            { "<leader>dt", "<cmd>DapViewToggle<cr>", desc = "DAP View Toggle" },
+            { "<leader>dvo", "<cmd>DapViewOpen<cr>", desc = "DAP View Open" },
+            { "<leader>dvc", "<cmd>DapViewClose<cr>", desc = "DAP View Close" },
+        },
+        config = function(_, opts)
+            require("dap-view").setup(opts)
+
+            vim.fn.sign_define(
+                "DapBreakpoint",
+                { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+                -- { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+            )
+            vim.fn.sign_define("DapBreakpointCondition", {
+                text = "",
+                texthl = "DapBreakpointCondition",
+                linehl = "DapBreakpointCondition",
+                numhl = "DapBreakpointCondition",
+            })
+            vim.fn.sign_define("DapBreakpointRejected", {
+                text = "󰅚",
+                texthl = "DapBreakpointRejected",
+                linehl = "DapBreakpointRejected",
+                numhl = "DapBreakpointRejected",
+            })
+            vim.fn.sign_define(
+                "DapLogPoint",
+                { text = "󰍹", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+            )
+            vim.fn.sign_define(
+                "DapStopped",
+                -- { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+                { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+            )
+
+            local dap = require("dap")
+            dap.listeners.after.event_initialized["dap-view"] = function()
+                vim.cmd("DapViewOpen")
+            end
+            dap.listeners.before.event_terminated["dap-view"] = function()
+                vim.cmd("DapViewClose")
+            end
+            dap.listeners.before.event_exited["dap-view"] = function()
+                vim.cmd("DapViewClose")
+            end
+        end,
+    },
+}
