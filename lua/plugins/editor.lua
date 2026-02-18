cf57b798ed035bb14feb4a04fca5aa12076aacd6
Ricardo Mora <rurimoca@gmail.com>
2026-02-18 00:15:10 -0600

    Formatea código

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,13 +1,6 @@
 return {
-    {
-        --"jiangmiao/auto-pairs",
-    },
-    {
-        "numToStr/Comment.nvim",
-        opts = {
-            padding = true,
-        },
-    },
+    -- { "jiangmiao/auto-pairs" },
+    { "numToStr/Comment.nvim", opts = { padding = true } },
     {
         "kylechui/nvim-surround",
         version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
