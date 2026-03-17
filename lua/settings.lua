c5a4130642093e68fec98337fbb940d267ab1b46
Ricardo Mora <rurimoca@gmail.com>
2026-03-16 23:35:36 -0600

    Agrega conceallevel para markdown

--- lua/settings.lua
+++ lua/settings.lua
@@ -68,6 +68,15 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt.formatoptions:remove({ "c", "r", "o" })
     end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("markdown_conceal", { clear = true }),
+    pattern = "markdown",
+    callback = function()
+        vim.opt_local.conceallevel = 2
+        vim.opt_local.concealcursor = "nc"
+    end,
+})
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
     group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
     callback = function()
